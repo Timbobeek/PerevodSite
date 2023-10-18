0e3e3671ac8b88e1e6fc6bc226cff70ed547f5ad
Timofey Goloshchapov <tim.goloschapov@gmail.com>
2023-10-18 14:52:36 -0500

    start

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,7 @@
+import project from "./project-schema";
+import page from "./page-schema";
+
+const schemas = [project, page];
+//all schemas are defined inside this file 
+
+export default schemas;
